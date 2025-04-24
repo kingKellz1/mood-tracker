494a9349708d8b525f7219438ae787af431fa23c
kingKellz1 <nicheal91@hotmail.com>
2025-04-24 17:41:00 -0400

    Updated python file

--- mood_tracker.py
+++ mood_tracker.py
@@ -2,37 +2,38 @@ import datetime
 from pathlib import Path
 
 #Gets the directory of the parent folder
-current_dir = Path(__file__).parent
+SCRIPT_DIR = Path(__file__).parent
 
 #Points to the file in the same directory
-file_path = current_dir / "mood_log.txt"
+file_path = SCRIPT_DIR / "mood_log.txt"
 
-if not file_path.exists():
+if not file_path.exists:
     file_path.touch()  # Creates an empty file if it doesn’t exist yet
 
 
 def view_mood_log(): 
-    '''Creates a function to view the log file'''
-    with open(file_path, "r") as file:
+    """Creates a function to view the log file"""
+    with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
             print(line.strip())
 
 def add_mood_log(): 
-    '''Creates a function so user can add a log enty'''
+    """Creates a function so user can add a log entry"""
     users_feeling = input("\nHow are you feeling today?: (Happy, Sad, Mad, Angry, Flat) \n")
     users_day = input("\nHow was your day?: \n")
 
     #Stores the current date and time to a variable called "now"
-    now = datetime.datetime.now() 
+    now = datetime.datetime.now()
     
     #Stores only the date in the variable
-    formated_now = now.strftime("%Y-%m-%d") 
+    formatted_now = now.strftime("%Y-%m-%d")
 
-    with open(file_path, "a") as file:
-        line = f"{formated_now} | {users_feeling} | {users_day}\n"
+    with open(file_path, "a", encoding="utf-8") as file:
+        line = f"{formatted_now} | {users_feeling} | {users_day}\n"
         file.write(line)
 
 def main():
+    """Calls the main function"""
     print("\n***********************************")
     print("Hello, Welcome to your mood tracker")
     print("***********************************\n")
@@ -46,7 +47,7 @@ def main():
             break
         else:
             #Prompts the user to enter a valid choice
-            print("Please make a valid choice!\n") 
+            print("Please make a valid choice!\n")
 
 if __name__ == "__main__":
     main()
